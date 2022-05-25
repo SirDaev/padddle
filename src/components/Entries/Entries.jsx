b5eaadb807644723ebfcb27e95dce27a6b5341d5
Dave Judd <52185dj@gmail.com>
2022-05-25 16:32:16 -0700

    Setup guesses structure

--- src/components/Entries/Entries.jsx
+++ src/components/Entries/Entries.jsx
@@ -2,7 +2,10 @@ import { useContext,useEffect,useState } from 'react'
 // Context
 import gameContext from '../../contexts/game/gameContext'
 // Style
-import { EntriesForm } from './Entries.styles';
+import {
+  EntriesForm,
+  Guess
+} from './Entries.styles';
 
 const Entries = () => {
   const { gameDispatch,gameState } = useContext(gameContext)
@@ -25,7 +28,6 @@ const Entries = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(currentGuess)
     gameDispatch({
       type: 'SET_GUESSES',
       payload: currentGuess
@@ -39,26 +41,22 @@ const Entries = () => {
   }
 
   useEffect(() => {
-    if(currentGuess === "") {
-      setDisabled(true);
-    } else {
-      setDisabled(false);
-    }
+    setDisabled(currentGuess === "" ? true : false);
   },[currentGuess]);
+
+  const fiveArr = ["","","","",""];
 
   return (
     <>
-      <ol>
-        {gameState.guesses.map((guess) => {
-          return (
-            <li key={guess.id}>{guess.text}</li>
-          )
-        })}
-      </ol>
-      <form onSubmit={handleSubmit}>
+      {fiveArr.map((item,index) => {
+        return(
+          <Guess key={index}>{gameState.guesses[index] ? gameState.guesses[index].text : ""}</Guess>
+        )
+      })}
+      <EntriesForm onSubmit={handleSubmit}>
         <input type="text" value={currentGuess} onChange={handleChange} />
         <button type="submit" disabled={disabled}>GUESS</button>
-      </form>
+      </EntriesForm>
     </>
   )
 }
--- src/components/Entries/Entries.styles.js
+++ src/components/Entries/Entries.styles.js
@@ -7,3 +7,15 @@ export const EntriesForm = styled.form`
     width: 100%;
   }
 `;
+
+export const Guess = styled.div`
+  padding: 10px;
+  margin-bottom: 10px;
+  background: #eee;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
